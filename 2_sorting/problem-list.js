e5efe2d515df9de041be4f4f5b14997065754d15
kimi <sarahkmcaleer@gmail.com>
2020-12-06 10:28:02 -0500

    sorting quicksort example problem with solution

--- 2_sorting/problem-list.js
+++ 2_sorting/problem-list.js
@@ -1,6 +1,15 @@
 //3 coding simple challenges
 
-//1)
+//1) From https://medium.com/@Charles_Stover/implementing-quicksort-in-javascript-8044a8e2bf39
+
+const quickSort = (
+    unsortedArray,
+    comparator = defaultComparator
+  ) => {
+
+    return sortedArray;
+  };
+
 
 
 
